9d2d7e0cf956ce0bc260282f71e72c42f6517519
Rupaj Sen <rupajps4@gmail.com>
2023-03-15 12:53:29 +0530

    Initial Commit

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Rupaj Sen on 15/03/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
